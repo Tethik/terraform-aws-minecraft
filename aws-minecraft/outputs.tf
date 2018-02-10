f604e3b209667d9aeefbff7dc798da18e4fec3ad
Joakim Uddholm <tethik@gmail.com>
2018-02-10 15:49:23 +0100

    Better outputs.

--- aws-minecraft/outputs.tf
+++ aws-minecraft/outputs.tf
@@ -1,3 +1,17 @@
 output "server_ip" {
-  value = "${aws_instance.server.*.public_ip}"
+  value = "${ var.server_count > 0 ? "${join("", aws_instance.server.*.public_ip)}" : "" }"
 }
+
+output "s3_bucket_arn" {
+  value = "${aws_s3_bucket.server_files.arn}"
+}
+
+output "s3_bucket_aws_access_key" {
+  value = "${aws_iam_access_key.admin_user.id}"
+}
+
+output "s3_bucket_aws_secret_key" {
+  value = "${aws_iam_access_key.admin_user.secret}"
+}
+
+
--- minecraft.tf
+++ minecraft.tf
@@ -1,5 +1,6 @@
 variable "aws_access_key" {}
 variable "aws_secret_key" {}
+variable "server_count" {}
 
 module "aws-minecraft" {
   source = "./aws-minecraft"
@@ -7,6 +8,18 @@ module "aws-minecraft" {
   server_name = "blacknode"
   aws_secret_key = "${var.aws_secret_key}"
   aws_access_key = "${var.aws_access_key}"
-  server_count = 1
+  server_count = "${var.server_count}"
 }
 
+output "minecraft_server_ip" {
+  value = "${module.aws-minecraft.server_ip}"
+}
+output "minecraft_s3" {
+  value = "${module.aws-minecraft.s3_bucket_arn}"
+}
+output "minecraft_s3_aws_access_key" {
+  value = "${module.aws-minecraft.s3_bucket_aws_access_key}"
+}
+output "minecraft_s3_aws_secret_key" {
+  value = "${module.aws-minecraft.s3_bucket_aws_secret_key}"
+}
